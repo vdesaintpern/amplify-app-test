2da746c19a496ba0eab7310282edc45422afc23f
Vivien de Saint Pern <vivieper@amazon.com>
2023-04-17 09:54:42 +0200

    show API url

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          API URL = {process.env.API_URL}
         </p>
         <a
           className="App-link"
